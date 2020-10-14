8ef6fd7196035bb04ae4eae9b3c072bddbe3a8c1
Yesi Meza <yesi.meza10@gmail.com>
2020-10-14 13:23:17 -0600

    Passes product tests

--- lib/product.rb
+++ lib/product.rb
@@ -5,8 +5,11 @@ attr_reader :category, :name, :unit_price, :quantity
     @category = category
     @name = name
     @unit_price = unit_price
-    @quantity = 10
+    @quantity = quantity.to_i
   end
 
-  
+  def total_price
+    @unit_price * @quantity
+  end
+
 end
