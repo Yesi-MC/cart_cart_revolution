cdc347cc051f19aeb50bb1fc23f882dffde57b16
Yesi Meza <yesi.meza10@gmail.com>
2020-10-14 13:18:29 -0600

    Create product class pass up to quantity test

---
+++ lib/product.rb
@@ -0,0 +1,12 @@
+class Product
+
+attr_reader :category, :name, :unit_price, :quantity
+  def initialize(category, name, unit_price, quantity)
+    @category = category
+    @name = name
+    @unit_price = unit_price
+    @quantity = 10
+  end
+
+  
+end
